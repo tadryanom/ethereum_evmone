19db4a38548d9d6212a9d7d5c084c931eb3385c1
Andrei Maiboroda <andrei@ethereum.org>
2021-10-05 16:49:10 +0200

    Tests for RJUMPI

--- test/unittests/evm_eof_test.cpp
+++ test/unittests/evm_eof_test.cpp
@@ -253,3 +253,69 @@ TEST_P(evm, eof2_rjump_0_offset)
     ASSERT_EQ(result.output_size, 1);
     EXPECT_EQ(result.output_data[0], 1);
 }
+
+TEST_P(evm, eof2_rjumpi)
+{
+    // Relative jumps are not implemented in Advanced.
+    if (isAdvanced())
+        return;
+
+    rev = EVMC_SHANGHAI;
+    auto code = eof1_bytecode(
+        rjumpi(10, calldataload(0)) + mstore8(0, 2) + ret(0, 1) + mstore8(0, 1) + ret(0, 1));
+
+    // RJUMPI condition is true
+    execute(code, "01");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 1);
+
+    // RJUMPI condition is false
+    execute(code, "00");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 2);
+}
+
+TEST_P(evm, eof2_rjumpi_backwards)
+{
+    // Relative jumps are not implemented in Advanced.
+    if (isAdvanced())
+        return;
+
+    rev = EVMC_SHANGHAI;
+    auto code = eof1_bytecode(rjump(11) + OP_INVALID + mstore8(0, 1) + ret(0, 1) +
+                              rjumpi(-16, calldataload(0)) + mstore8(0, 2) + ret(0, 1));
+
+    // RJUMPI condition is true
+    execute(code, "01");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 1);
+
+    // RJUMPI condition is false
+    execute(code, "00");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+    EXPECT_EQ(result.output_data[0], 2);
+}
+
+TEST_P(evm, eof2_rjumpi_0_offset)
+{
+    // Relative jumps are not implemented in Advanced.
+    if (isAdvanced())
+        return;
+
+    rev = EVMC_SHANGHAI;
+    auto code = eof1_bytecode(rjumpi(0, calldataload(0)) + mstore8(0, 1) + ret(0, 1));
+
+    // RJUMPI condition is true
+    execute(code, "01");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+
+    // RJUMPI condition is false
+    execute(code, "01");
+    EXPECT_STATUS(EVMC_SUCCESS);
+    ASSERT_EQ(result.output_size, 1);
+}
